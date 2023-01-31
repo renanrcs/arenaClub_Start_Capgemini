93e24b16ed3c55e836cf22b9aeedf21bea7d3415
izabella-araujo <mariaizabellasda@gmai.com>
2023-01-31 11:32:27 -0300

    arena

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
+import { DetalhesServicoComponent } from './servico/detalhes-servico/detalhes-servico.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'servico', loadChildren: () => import('./servico/servico.module').then(m => m.ServicoModule) },
+  { path: "", redirectTo: "servico", pathMatch: "full"},
+  { path: ':id', component: DetalhesServicoComponent },
+  { path: "**", component: NaoEncontradoComponent}
+];
 
 @NgModule({
+  declarations:[],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
